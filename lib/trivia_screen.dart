7e2a2c1f0dcc1d2db9996e3d74315e95dcc179a3
Herbert Nordson <herbert_nordson@hotmail.com>
2021-02-04 14:32:31 -0300

    Includes: Foi inserido no APP a stado de resposta

--- lib/trivia_screen.dart
+++ lib/trivia_screen.dart
@@ -6,6 +6,8 @@ class TriviaScreen extends StatefulWidget {
 }
 
 class _TriviaScreenState extends State<TriviaScreen> {
+  int answer = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,10 +26,72 @@ class _TriviaScreenState extends State<TriviaScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('PERGNTA 1', style: TextStyle(fontSize: 16,),),
-                SizedBox(height: 16,),
+                Text(
+                  'PERGNTA 1',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
                 Text('O que é Flutter?'),
-              ],),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(1, 3),
+                      ),
+                    ],
+                  ),
+                  child: RadioListTile(
+                      value: 1,
+                      groupValue: answer,
+                      title: Text('É um framework da Google.'),
+                      onChanged: (int value) {
+                        setState(() {
+                          answer = value;
+                        });
+                      }),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(1, 3),
+                      ),
+                    ],
+                  ),
+                  child: RadioListTile(
+                      value: 2,
+                      groupValue: answer,
+                      title: Text('É um novo Sistema Operacional.'),
+                      onChanged: (int value) {
+                        setState(() {
+                          answer = value;
+                        });
+                      }),
+                ),
+              ],
+            ),
           ),
         ],
       ),
